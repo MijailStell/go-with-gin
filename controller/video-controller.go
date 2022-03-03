206b588e294392a61e448a29b17f798ca00dd591
mijailstell <mijailstell@gmail.com>
2022-03-03 09:52:26 -0500

    add custom validators

--- controller/video-controller.go
+++ controller/video-controller.go
@@ -3,7 +3,9 @@ package controller
 import (
 	"github.com/MijailStell/go-with-gin/entity"
 	"github.com/MijailStell/go-with-gin/service"
+	customValidators "github.com/MijailStell/go-with-gin/validators"
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 )
 
 type VideoController interface {
@@ -15,7 +17,11 @@ type videoController struct {
 	service service.VideoService
 }
 
+var validate *validator.Validate
+
 func New(service service.VideoService) VideoController {
+	validate = validator.New()
+	validate.RegisterValidation("is-gmail", customValidators.ValidateIsGmail)
 	return &videoController{
 		service: service,
 	}
@@ -28,6 +34,10 @@ func (controller *videoController) FindAll() []entity.Video {
 func (controller *videoController) Save(context *gin.Context) error {
 	var video entity.Video
 	err := context.ShouldBindJSON(&video)
+	if err != nil {
+		return err
+	}
+	err = validate.Struct(video)
 	if err != nil {
 		return err
 	}
--- entity/person.go
+++ entity/person.go
@@ -3,6 +3,6 @@ package entity
 type Person struct {
 	FirstName string `json:"firstName" binding:"required"`
 	LastName  string `json:"lastName" binding:"required"`
-	Age       string `json:"age" binding:"gte=1,lte=130"`
-	Email     string `json:"email" validate:"required,email"`
+	Age       int8   `json:"age" binding:"gte=1,lte=130"`
+	Email     string `json:"email" validate:"required,email,is-gmail"`
 }
